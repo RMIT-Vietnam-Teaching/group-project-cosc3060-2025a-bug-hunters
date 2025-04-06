7e5b5edf56b80332e8f962ac2f43e6d452109d84
s3818206 <s3818206@rmit.edu.vn>
2025-04-07 02:27:18 +0700

    First structure built up for the whole team to follow

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-console.log("Hello world")
+const express = require("express");
+const chalk = require("chalk");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+const connectDB = require("./utils/db");
+
+const { port } = require("./configs/keys");
+
+dotenv.config();
+
+const app = express();
+
+//set up view engine
+app.set("view engine", "ejs");
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+// Set up Mongo DB connection
+connectDB();
+
+// Import Routes
+const authRoutes = require("./routes/authRoutes");
+const homeRoutes = require("./routes/homeRoutes");
+
+app.use("/", homeRoutes);
+app.use("/auth", authRoutes);
+
+app.listen(port, () => {
+    console.log(chalk.green(`Server is running on port ${port}`));
+});
