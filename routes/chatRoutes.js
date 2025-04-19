d08b406b3bc594b49908a82aacf9def6e5c6c686
KhoaDUyNguyen1 <177729348+KhoaDUyNguyen1@users.noreply.github.com>
2025-04-19 14:54:45 +0700

    deleteing unneccesary routes

--- routes/chatRoutes.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-
-router.get("/message", (req, res) => {
-    res.render("message"); // ← this is key
-});
-
-module.exports = router;
--- routes/instructorRoutes.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-
-router.get("/instructorUpload", (req, res) => {
-    res.render("instrucPage"); 
-});
-
-module.exports = router;
