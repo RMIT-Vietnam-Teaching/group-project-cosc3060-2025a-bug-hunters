fb056c441b4c4a2c0633568d02895ca7912af5d5
Joanna4444 <s4026688@rmit.edu.vn>
2025-05-11 01:10:06 +0700

    fixing checkout page to link the value in userSetting balance

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -1,20 +1,28 @@
 const User = require('../models/User');
 
 exports.renderCheckoutPage = async (req, res) => {
-    try {
-        const cartItems = req.session.cart || [];
-        const routeUserId = req.query.id; 
-        const routeUser = await User.findById(routeUserId);
-        if (!routeUser) return res.redirect("/auth/login");
-        res.render('checkout', {
-            cartItems,
-            routeUser
-        });
-      } catch (error) {
-        console.error('Error rendering checkout page:', error.message, error.stack);
-    
-        res.status(500).send('Internal Server Error');
-      }};
+  try {
+    const routeUserId = req.query.id; 
+    const routeUser = await User.findById(routeUserId);  
+
+    if (!routeUser) return res.redirect("/auth/login");
+
+    const cardInfo = routeUser.cardPaymentInfo || {}; 
+
+    const cartItems = req.session.cart || [];
+
+    res.render("checkout", {
+      cartItems,
+      user: routeUser,
+      cardInfo,
+    });
+  } catch (err) {
+    console.error("Error rendering checkout page:", err.message, err.stack);
+    res.status(500).send("Server Error");
+  }
+};
+
+
 
 
 exports.renderAddCoin = async (req, res) => {
