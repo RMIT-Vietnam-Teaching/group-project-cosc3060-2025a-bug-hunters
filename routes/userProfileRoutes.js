bc7ed45ad12292c18bb5a164a29fc894d0ac68b0
Joanna4444 <s4026688@rmit.edu.vn>
2025-05-14 23:54:31 +0700

    fsfeji

--- routes/userProfileRoutes.js
+++ routes/userProfileRoutes.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const { renderUserProfile } = require("../controllers/userProfile");
-const { requireOwnUserAccess } = require("../middlewares/auth");
+const { followUser, unfollowUser } = require("../controllers/follow");
+
 
 router.get("/:userRouteId", renderUserProfile);
+router.post("/follow/:userId", followUser);
+router.post( "/unfollow/:userId", unfollowUser);
 
 module.exports = router;
